75697873763d3ce807aa659d8bfc66a0acba559c
Hanaa Sadoqi <hsadoqi@gmail.com>
2018-12-04 16:42:43 -0500

    played with widget

--- src/UploadPhoto/UploadPhoto.js
+++ src/UploadPhoto/UploadPhoto.js
@@ -1,28 +1,32 @@
 import React, { Component } from 'react'
+import './UploadPhoto.css'
 
+let image
 export default class UploadPhoto extends Component {
+    state = {
+        photo: null
+    }
 
-  checkUploadResult = (resultEvent) => {
+
+
+    showWidget = (widget) =>{
+        widget.open()
+      }
+
+    checkUploadResult = (resultEvent) => {
     // console.log("Steven holla",resultEvent);
     if (resultEvent.event === 'success'){
         // Call helper function to post photo in the backend
         // example below
-      this.postPhoto(
-
-        {poster: this.state.user,
-          caption:'yass bitch',
-          img: resultEvent.info.secure_url}
-
-      )
+        this.setState({ photo: resultEvent.info.secure_url})
+        // this.postPhoto({caption:'yass bitch', img: resultEvent.info.secure_url})
       // route them to the necesary place
-      //   .then(this.props.history.push(`/profile`))
+        // .then(this.props.history.push(`/profile`))
+        }
     }
-  }
 
-  postPhoto = (userObj) =>{
-
+    postPhoto = (userObj) =>{
     let token = localStorage.getItem('token')
-
     fetch('http://localhost:3000/images', {
       method: 'POST',
       headers: {
@@ -37,24 +41,24 @@ export default class UploadPhoto extends Component {
       })
     })
     .then(r => r.json())
-    .then(console.log)
-  }
-
-
-  showWidget = (widget) =>{
-    widget.open()
-  }
+    }
 
 
   // CloudName:"df3knd9zd",
   // uploadPreset:"rfk3xygs"
     render(){
         console.log(this.state)
-        let widget = window.cloudinary.createUploadWidget({
-  cloudName: "df3knd9zd", uploadPreset: "rfk3xygs" }, (error, result) => {this.checkUploadResult(result) });
+        let widget = window.cloudinary.createUploadWidget({cloudName: "df3knd9zd", uploadPreset: "rfk3xygs" }, (error, result) => {this.checkUploadResult(result) });
+
+        if(this.state.photo){
+            image = <img src={this.state.photo} alt=""/>
+            widget.close({ quiet:true })
+        }
+
         return (
-            <div>
-              <button onClick={()=> this.showWidget(widget)}>UploadPhoto </button>
+            <div className='upload-photo'>
+                {image}
+                <button onClick={()=> this.showWidget(widget)}>UploadPhoto </button>
             </div>
         )
     }
